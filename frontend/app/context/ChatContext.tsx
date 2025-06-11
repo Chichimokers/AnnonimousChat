82d16dc7f12d03c1a2416c4569b459d688f3a1c0
Chichimokres <developer1575@gmail.com>
2025-06-11 01:21:09 -0400

    ss

--- frontend/app/context/ChatContext.tsx
+++ frontend/app/context/ChatContext.tsx
@@ -42,7 +42,7 @@ export const ChatProvider = ({ children }: { children: React.ReactNode }) => {
     
     }
 
-    const socket = new WebSocket('wss://esaki-jrr.com:3001');
+    const socket = new WebSocket('wss://esaki-jrr.com/websocket/');
     socketRef.current = socket;
 
     socket.onopen = () => {
