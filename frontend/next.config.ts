d54b3446323217701459d652d7240ebd67b39681
Chichimokres <developer1575@gmail.com>
2025-06-11 00:43:01 -0400

    gg

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath:'/chat'
+  basePath:'/chat',
+  assetPrefix:'/chat/'
 };
 
 export default nextConfig;
