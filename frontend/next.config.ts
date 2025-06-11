e1771341424400dbebc3e3f7e3a4f2336601ee9a
Chichimokres <developer1575@gmail.com>
2025-06-11 01:15:53 -0400

    ssss

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
+  assetPrefix:'/chat/',
   allowedDevOrigins:["167.99.238.54","esaki-jrr.com"]
 };
 
